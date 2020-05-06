7639a5c2228a81ac3a29a2492afea6757a04f406
ThamizhselvanET <34275155+ThamizhselvanET@users.noreply.github.com>
2020-05-06 11:28:03 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -28,6 +28,6 @@ docker run --rm \
     --scan /src \
     --format "ALL" \
     --project "$DC_PROJECT" \
-    --out /OWASP-Dependency-Check/reports/
+    --out /OWASP-Dependency-Check/reports
     # Use suppression like this: (where /src == $pwd)
     # --suppression "/src/security/dependency-check-suppression.xml"
